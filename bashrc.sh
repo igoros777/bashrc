fc3ad9e0a899bdedc905790c91548f304308e0ee
Igor Os <ichmod444@gmail.com>
2021-06-14 16:01:01 -0400

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -914,4 +914,64 @@ alias help='/usr/bin/tldr'
 # Example:
 # help rsync
 
+#/*       _\|/_
+#         (o o)
+# +----oOO-{_}-OOo-------------------------------------------------------------+
+# |Show keyboard showrtcuts for bash productivity                              |
+# +---------------------------------------------------------------------------*/
+
+shelp() {
+  cat << EOF
+#  Navigation
+#  ------------------
+#  Ctrl + a            Go to the beginning of the line.
+#  Ctrl + e            Go to the end of the line.
+#  Alt + f             Move the cursor forward one word.
+#  Alt + b             Move the cursor back one word.
+#  Ctrl + f            Move the cursor forward one character.
+#  Ctrl + b            Move the cursor back one character.
+#  Ctrl + x, x         Toggle between the current cursor position and the
+#                      beginning of the line.
+
+#  Editing
+#  ------------------
+#  Ctrl + _            Undo (within reason)
+#  Ctrl + x, Ctrl + e  Edit the current command in your $EDITOR.
+#  Alt + d             Delete the word after the cursor.
+#  Alt + Delete        Delete the word before the cursor.
+#  Ctrl + d            Delete the character beneath the cursor.
+#  Ctrl + h            Delete the character before the cursor (like backspace).
+#  Ctrl + k            Cut the line after the cursor to the clipboard.
+#  Ctrl + u            Cut the line before the cursor to the clipboard.
+#  Ctrl + d            Cut the word after the cursor to the clipboard.
+#  Ctrl + w            Cut the word before the cursor to the clipboard.
+#  Ctrl + y            Paste the last item to be cut.
+
+#  Processes
+#  ------------------
+#  Ctrl + l            Clear the entire screen (like the clear command).
+#  Ctrl + z            Place the currently running process into a suspended
+#                      background process (and then use fg to restore it).
+#  Ctrl + c            Kill the currently running process by sending the SIGINT
+#                      signal.
+#  Ctrl + d            Exit the current shell.
+#  Enter, ~, .         Exit a stalled SSH session.
+
+#  History
+#  ------------------
+#  Ctrl + r            Bring up the history search.
+#  Ctrl + g            Exit the history search.
+#  Ctrl + p            See the previous command in the history.
+#  Ctrl + n            See the next command in the history.
+EOF
+}
+
+# The command below will run the `shelp` alias when you press the F12 key. In
+# my case, pressing the F12 was showing "4~" in the terminal window and so
+# that's what I used. Your terminal emulator may be different, so just hit F12,
+# see what you get, and insert that string in place of my "4~".
+
+bind '"4~":"shelp\n"'
+
+
 #   _,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,
